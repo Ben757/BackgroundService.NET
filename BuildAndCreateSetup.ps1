084f94538147e20a5bf05465bbda7573429b5ca2
Benjamin Riedle <riedle.benjamin@gmail.com>
2022-02-10 13:22:31 +0100

    Add powershell script to build and bundle the service in a msi including your plugin

---
+++ BuildAndCreateSetup.ps1
@@ -0,0 +1,22 @@
+$pluginProjectPath=$args[0]
+$pluginOutputDir= -join($args[1],"\*")
+
+Write-Host "Building Backgroundservice"
+
+dotnet publish -c Release "BackgroundService.NET\\BackgroundService.NET.csproj"
+
+
+Write-Host "Building project $pluginProjectPath"
+
+dotnet publish -c Release $pluginProjectPath
+
+
+Write-Host "Copying $pluginOutputDir to Plugins dir"
+
+Copy-Item -Path $pluginOutputDir -Destination "BackgroundService.NET\\publish\\Plugins"
+
+Write-Host "Creating Setup"
+
+MSBuild.exe -p:Configuration=Release "BackgroundService.NET.Setup\\BackgroundService.NET.Setup.wixproj"
+
+Write-Host "Finished"
