58cd469a2dd7b82a70706f1bee475a2cf077c8ff
Benjamin Riedle <riedle.benjamin@gmail.com>
2022-02-10 09:11:41 +0100

    Add first sample to test CronJobProvider
    Be sure to copy CronJob.deps.json as well to output directory of BackgroundService.NET

---
+++ BackgroundService.NET.SamplePlugins/BackgroundService.NET.HelloWorldPlugin/HelloWorldJob.cs
@@ -0,0 +1,14 @@
+﻿using BackgroundService.NET.CronJob;
+
+namespace BackgroundService.NET.HelloWorldPlugin;
+
+// ReSharper disable once UnusedType.Global
+public class HelloWorldJob : ICronJob
+{
+    public Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Hello World at {DateTimeOffset.Now}");
+        
+        return Task.CompletedTask;
+    }
+}
