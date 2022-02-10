a6e987ce5e3647eab32eedb5432676251309da20
Benjamin Riedle <riedle.benjamin@gmail.com>
2022-02-10 13:44:01 +0100

    Added NLog to enable file logging

--- BackgroundService.NET/Program.cs
+++ BackgroundService.NET/Program.cs
@@ -1,6 +1,8 @@
 using BackgroundService.NET;
 using BackgroundService.NET.CronHostService;
 using BackgroundService.NET.CronJobProviderService;
+using NLog;
+using NLog.Extensions.Logging;
 
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(ConfigureServices)
@@ -19,4 +21,12 @@ void ConfigureServices(HostBuilderContext context, IServiceCollection services)
 
     services.Configure<CronHostOptions>(context.Configuration.GetSection(nameof(CronHostOptions)));
     services.Configure<CronJobProviderOptions>(context.Configuration.GetSection(nameof(CronJobProviderOptions)));
+
+    services.AddSingleton<ILoggerFactory>(CreateNLogFactory(context.Configuration.GetSection("NLog")));
+}
+
+ILoggerFactory CreateNLogFactory(IConfigurationSection nLogConfiguration)
+{
+    LogManager.Configuration = new NLogLoggingConfiguration(nLogConfiguration);
+    return new NLogLoggerFactory(new NLogLoggerProvider(new NLogProviderOptions(), LogManager.LogFactory));
 }
