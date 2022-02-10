753e8ee5c8ca5a5932010c71c1fa15f08cf97bcc
Benjamin Riedle <riedle.benjamin@gmail.com>
2022-02-10 09:37:52 +0100

    Add another sample plugin to test project references
    - References a project instead of a NuGet package
    - Verifies that project references work as well

---
+++ BackgroundService.NET.SamplePlugins/BackgroundService.NET.PluginProjectReference/PluginHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace BackgroundService.NET.PluginProjectReference;
+
+public class PluginHelper
+{
+    public void Print()
+    {
+        Console.WriteLine($"{DateTimeOffset.Now} This is printed from the reference project");
+    }
+}
---
+++ BackgroundService.NET.SamplePlugins/BackgroundService.NET.PluginWithProjectReference/PluginWithProjectReferenceJob.cs
@@ -0,0 +1,14 @@
+﻿using BackgroundService.NET.CronJob;
+using BackgroundService.NET.PluginProjectReference;
+
+namespace BackgroundService.NET.PluginWithProjectReference;
+
+public class PluginWithProjectReferenceJob : ICronJob
+{
+    public Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        new PluginHelper().Print();
+        
+        return Task.CompletedTask;
+    }
+}
