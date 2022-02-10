f1ffbdb2c91e761c1e1bc52e200d5f402627add3
Benjamin Riedle <riedle.benjamin@gmail.com>
2022-02-10 09:31:48 +0100

    Added two more sample plugins to test the system
    PluginWithDependencies:
    - Adds a NuGet reference to Newtonsoft.Json
    - Verifies that NuGet references work
    - Be sure to copy all necessary assemblies to the output folder of BackgroundService.NET
    
    FailingPlugin
    - Randomly fails and throws an exception
    - Adds true async behaviour using a random delay
    - Verifies that failing jobs do not break the service

---
+++ BackgroundService.NET.SamplePlugins/BackgroundService.NET.FailingPlugin/RandomFailingJob.cs
@@ -0,0 +1,23 @@
+﻿using BackgroundService.NET.CronJob;
+
+namespace BackgroundService.NET.FailingPlugin;
+
+// ReSharper disable once UnusedType.Global
+public class RandomFailingJob : ICronJob
+{
+    private readonly Random random = new();
+
+    public async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        var randomDelay = random.Next(10);
+
+        await Task.Delay(TimeSpan.FromMilliseconds(100 * randomDelay), cancellationToken);
+
+        var shouldFail = random.Next(100) < 50;
+
+        if (shouldFail)
+            throw new InvalidOperationException("Failed randomly");
+        
+        Console.WriteLine($"{DateTimeOffset.Now}: Job completed successfully");
+    }
+}
---
+++ BackgroundService.NET.SamplePlugins/BackgroundService.NET.PluginWithDependencies/JsonDependentJob.cs
@@ -0,0 +1,26 @@
+﻿using BackgroundService.NET.CronJob;
+using Newtonsoft.Json;
+
+namespace BackgroundService.NET.PluginWithDependencies;
+
+// ReSharper disable once UnusedType.Global
+public class JsonDependentJob : ICronJob
+{
+    public Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        var sampleObject = new
+        {
+            Id = Guid.NewGuid(),
+            SomeProperty = new
+            {
+                Text = "HelloWorld"
+            }
+        };
+
+        var json = JsonConvert.SerializeObject(sampleObject);
+        
+        Console.WriteLine($"{DateTimeOffset.Now}: {json}");
+        
+        return Task.CompletedTask;
+    }
+}
